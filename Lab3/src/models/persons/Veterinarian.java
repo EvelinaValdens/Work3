c6702435811fe34a1a1b848aa88bf6fc19552909
Eva <155992147+EvelinaValdens@users.noreply.github.com>
2024-01-15 22:48:33 +0300

    Update Veterinarian.java

--- Lab3/src/models/persons/Veterinarian.java
+++ Lab3/src/models/persons/Veterinarian.java
@@ -1,10 +1,10 @@
 package models.persons;
-
+import enums.Profession;
 import models.animals.Animal;
 
 public class Veterinarian extends Person{
-    public Veterinarian(String name) {
-        super(name);
+    public Veterinarian(String name, Profession profession) {
+        super(name, profession);
     }
     public void scheduleAppointment(Animal animal) {
         System.out.println(getName() + " назначил встречу для " + animal.getName() + ".");
