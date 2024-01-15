a89eeb8b37149577099c67fbf6d2c47bb48e807d
Eva <155992147+EvelinaValdens@users.noreply.github.com>
2024-01-15 20:57:04 +0300

    Update Doctor.java

--- Lab3/src/models/persons/Doctor.java
+++ Lab3/src/models/persons/Doctor.java
@@ -4,9 +4,6 @@ public class Doctor extends Person {
     public Doctor(String name) {
         super(name);
     }
-    public void shareWisdom(String wisdom) {
-        System.out.println(getName() + " делится своими знаниями: \"" + wisdom + "\"");
-    }
     public class ProfessionalLife {
         public static void reflectOnMedicalKnowledge() {
             System.out.println("Медицинские знания: Смерть так же естественна, как рождение.");
