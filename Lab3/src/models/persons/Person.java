4cee343c8f4217e8e253cc0364cfcdc74298ab65
Eva <155992147+EvelinaValdens@users.noreply.github.com>
2024-01-16 02:27:45 +0300

    Update Person.java
    Переделан класс Person. Теперь при некоторых методах персонажи меняют настроение

--- Lab3/src/models/persons/Person.java
+++ Lab3/src/models/persons/Person.java
@@ -1,37 +1,50 @@
 package models.persons;
 
-public abstract class Person {
-    protected String name;
+import enums.Feellings;
+import enums.Profession;
 
-   private Profession profession;
+public abstract class Person {
+    private String name;
+    private Profession profession;
+
+    private Feellings feellings;
 
     public Person(String name, Profession profession) {
         this.name = name;
-        this.profession = profession;
+        this.profession = Profession.UNKNOWN;
+        feellings = Feellings.NOTHING;
     }
+
+
     public void speak(String words) {
         System.out.println(name + " говорит, \"" + words + "\"");
     }
 
-    public void laugh() {
+    public Feellings getFeellings(){return feellings;}
+    public void setFeellings(Feellings feellings) { this.feellings = Feellings.NOTHING; }
+    public void laugh () {
+        setFeellings(Feellings.HAPPY);
         System.out.println(name + " смеется.");
     }
 
     public void think(String thoughts) {
+        setFeellings(Feellings.SAD);
         System.out.println(name + " думает, \"" + thoughts + "\"");
     }
 
     public String getName() {
         return name;
     }
-     public Profession getProfession(){
-         return profession;
-     }
-
-
     public void setName(String name) {
         this.name = name;
     }
+
+    public Profession getProfession(){return profession;}
+    public void setProfession() {
+        this.profession = Profession.UNKNOWN;
+    }
+
+
 
     @Override
     public String toString() {
@@ -40,3 +53,4 @@ public abstract class Person {
                 '}';
     }
 }
+
