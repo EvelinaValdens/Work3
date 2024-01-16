ed8384e7b6a513878e60b5b253d0f7b164a8b5ac
Eva <155992147+EvelinaValdens@users.noreply.github.com>
2024-01-16 13:01:46 +0300

    Update Doctor.java

--- Lab3/src/models/persons/Doctor.java
+++ Lab3/src/models/persons/Doctor.java
@@ -1,19 +1,32 @@
 package models.persons;
 
+import enums.Feellings;
+import enums.Profession;
+
 public class Doctor extends Person {
-    public Doctor(String name) {
-        super(name);
+    public Doctor(String name, Profession profession) {
+        super(name, profession);
     }
-    public class ProfessionalLife {
+    public void shareWisdom(String wisdom) {
+        setFeellings(Feellings.NOTHING);
+        System.out.println(getName() + " делится своими знаниями: \"" + wisdom + "\"");
+    }
+    public static class ProfessionalLife {
         public static void reflectOnMedicalKnowledge() {
             System.out.println("Медицинские знания: Смерть так же естественна, как рождение.");
         }
+
     }
     public class PersonalMemories {
         private String memory;
 
         public PersonalMemories(String memory) {
+            setFeellings(Feellings.MOODSTAYCALM);
             this.memory = memory;
+        }
+
+        public void recallMemory() {System.out.println("Вспоминая: " + memory);
         }
     }
 }
+
